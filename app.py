9eb439c2c96105ff51e0e756bb936855c1b34d22
Derrick <derrickchen0201@gmail.com>
2025-02-22 23:32:24 -0500

    added logo

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('home.html')
-
-@app.route('/')
 def home_page():
     return render_template('home.html') # Nice!!!
+
+@app.route('/about')
+def results_page():
+    return render_template('results.html')
 
 if __name__ == '__main__':
     app.run(degub=True)
