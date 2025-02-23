75505934e377e18cbc04753c92b843f446986b97
nahumfisseha <nahumfisseha4045@gmail.com>
2025-02-22 22:58:39 -0500

    Update

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+@app.route('/redirect')
+def redirect_to_home():
+    return redirect(url_for('home'))  # Redirects to the home page
 @app.route('/')
 def home():
-    return "Hello Derrick!!!"
+    render_template('home.html') # Nice!!
 
 if __name__ == '__main__':
     app.run(degub=True)
